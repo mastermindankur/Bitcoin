8df76ce493d8e8abfd15bf7066bc280220303231
unknown <mandakineeiitkgp@gmail.com>
2015-10-19 13:19:11 +0530

    commit

--- src/PropogationDelay.java
+++ src/PropogationDelay.java
@@ -25,18 +25,24 @@ public class PropogationDelay {
         Thread.sleep(5000);
         List <Peer> peers= pg.getConnectedPeers();
         System.out.println(" ----------LIST OF PEERS ---------------");
-        for (int i = 0; i < peers.size(); i++) {
+        
+        int i=0;
+        for (i = 0; i < peers.size(); i++) {
 			System.out.println(peers.get(i).getAddress());
         }
         
 		 TransactionConfidence tc= tx.getConfidence();
 		 System.out.println("Transaction Confidence is "+ tc.getConfidenceType());
         
+		 int broadcastCount=0;
 		 java.util.HashSet <PeerAddress> pas=  (HashSet<PeerAddress>) tc.getBroadcastBy();
 		 System.out.println(" ----------LIST OF PEERS that have broadcasted Tx ---------------");
 		 for (PeerAddress pa : pas) {
 			    System.out.println(pa.getAddr());
+			    broadcastCount++;
 			}
+		 
+		 System.out.println("Out of +"+i+ " peers "+ broadcastCount+" peers have broadcasted the transaction.");
 
 		 
         }//end of while
